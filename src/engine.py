25800a58402e89c0e20e875937acd18876aa999e
davidkowalk <david.kowalk@gmail.com>
2022-08-26 08:53:25 +0200

    Added power calculation

---
+++ src/engine.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def get_powers(base, goal):
+    """
+    Arguments:
+        base    List of base vectors in row form
+        goal    Goal vector as list or array in row form
+
+    Returns:
+        error code
+        list of powers
+
+    Error Codes:
+        0   Normal execution
+        1   Base not linearily independent
+    """
+    #write base as matrix
+    # transpose all base vectors into col-vectors
+    base_vec = list()
+
+    for row_vec in base:
+        col_vec = np.transpose([row_vec])
+        base_vec.append(col_vec)
+
+    M = np.concatenate(base_vec, axis=1)
+
+    #Test independence
+    if np.linalg.det(M) == 0:
+        return 1, None
+
+    #invert Matrix
+    N = np.linalg.inv(M)
+
+    # calculate powers
+    return 0, np.dot(N , goal)
