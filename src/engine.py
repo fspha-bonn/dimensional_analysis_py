7f15dc7b4d349ccda08165322dfd0d0b300142f5
davidkowalk <david.kowalk@gmail.com>
2022-10-05 10:32:45 +0200

    Added Base Completion

--- src/engine.py
+++ src/engine.py
@@ -1,5 +1,70 @@
 import numpy as np
 
+def invertible(vectors):
+    # Generate matrix by padding column vectors together
+    matrix = np.concatenate([vectors], axis = 1).transpose()
+    dims = matrix.shape
+
+    if not dims[0] == dims[1]:
+        raise ValueError("Inappropriate amount of vectors supplied,\nor vectors of wrong shape. Matrix not invertible")
+        return
+
+    return not (np.linalg.det(matrix) == 0)
+
+def __iterate_unique_indices__(vec, max):
+    """
+    Takes vector of indices and iterates to the next unique index-combination.
+    """
+    i = -1
+    while i >= -len(vec):
+        vec[i] += 1
+        if not vec[i] > max:
+            break
+
+        vec[i] = 0
+        i -= 1
+
+    if len(vec) == len(np.unique(vec)):
+        return vec
+    else:
+        return __iterate_unique_indices__(vec, max)
+
+
+def complete_base(vectors, dim = 7):
+    """
+    Takes vectors and returns completed base as list of vectors.
+    """
+
+    n = len(vectors)
+
+    # pick vector from producing system
+    # and check whether the resulting system is
+    # linerarly independent. Repeat dim-n times
+    E = np.zeros((dim,dim))
+    for i in range(dim):
+        E[i][i] = 1
+
+    #pick dim-n vectors
+    n = dim-len(vectors)
+    indices = np.zeros(n)
+    for i in range(n):
+        indices[i] = i
+
+
+    is_invertible = False
+
+    while not is_invertible:
+        vec = list(vectors.copy())
+        for i in range(n):
+            vec.append(E[:,int(indices[i])])
+
+        is_invertible = invertible(vec)
+        if not is_invertible:
+            indices = __iterate_unique_indices__(indices, dim-1)
+
+    return np.concatenate([vec], axis = 1).transpose()
+
+
 def analyze(base, goal):
     """
     Arguments:
